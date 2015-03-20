66e63f1db47690aec00dbfe56d709b7495092520
carlossalazar <carlos_3598@hotmail.com>
2015-03-19 19:05:28 -0600

    Initial Commit

---
+++ myapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myapp
+//
+//  Created by Carlos Salazar on 3/19/15.
+//  Copyright (c) 2015 Carlos Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myapp
+//
+//  Created by Carlos Salazar on 3/19/15.
+//  Copyright (c) 2015 Carlos Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
