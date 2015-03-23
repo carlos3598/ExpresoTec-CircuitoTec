3f2b24f37f2cdc571b87e6c64dee9424edb19895
carlossalazar <carlos_3598@hotmail.com>
2015-03-23 12:25:16 -0600

    Commit inicial

---
+++ myapp/valle1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  valle1ViewController.h
+//  myapp
+//
+//  Created by Carlos Salazar on 3/23/15.
+//  Copyright (c) 2015 Carlos Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface valle1ViewController : UIViewController
+
+@end
---
+++ myapp/valle1ViewController.m
@@ -0,0 +1,83 @@
+//
+//  valle1ViewController.m
+//  myapp
+//
+//  Created by Carlos Salazar on 3/23/15.
+//  Copyright (c) 2015 Carlos Salazar. All rights reserved.
+//
+
+#import "valle1ViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
+
+
+@interface valle1ViewController ()
+
+@end
+@implementation valle1ViewController {
+    GMSMapView *mapView_;
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+                                                            longitude:151.20
+                                                                 zoom:6];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.myLocationEnabled = YES;
+    self.view = mapView_;
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.title = @"Sydney";
+    marker.snippet = @"Australia";
+    marker.map = mapView_;
+    mapView_.settings.myLocationButton = YES;
+    
+    GMSMutablePath *path = [GMSMutablePath path];
+    [path addLatitude:-33.866 longitude:151.195]; // Sydney
+    GMSMarker *fiji = [[GMSMarker alloc] init];
+    fiji.position = CLLocationCoordinate2DMake(-18.142, 178.431);
+    fiji.title = @"Fiji";
+    fiji.map = mapView_;
+    [path addLatitude:-18.142 longitude:178.431]; // Fiji
+    
+    GMSMarker *hawaii = [[GMSMarker alloc] init];
+    hawaii.position = CLLocationCoordinate2DMake(22.291, -158.821);
+    hawaii.title = @"Hawaii";
+    hawaii.map = mapView_;
+    [path addLatitude:21.291 longitude:-157.821]; // Hawaii
+    
+    GMSMarker *MontaunView = [[GMSMarker alloc] init];
+    MontaunView.position = CLLocationCoordinate2DMake(37.423, -122.091);
+    MontaunView.title = @"Montaun View";
+    MontaunView.map = mapView_;
+    [path addLatitude:37.423 longitude:-122.091]; // Mountain View
+    
+    GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
+    polyline.strokeColor = [UIColor blueColor];
+    polyline.strokeWidth = 5.f;
+    polyline.map = mapView_;
+    NSLog(@"User's location: %@", mapView_.myLocation);
+    self.view = mapView_;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
